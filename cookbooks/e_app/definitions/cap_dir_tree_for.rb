29c7581a74e71938745cfc3541c9d5ce7e8e04c9
tungpt247 <tungpt247@gmail.com>
2015-04-16 23:57:10 +0700

    correct unicorn PID locate

--- cookbooks/e_app/definitions/cap_dir_tree_for.rb
+++ cookbooks/e_app/definitions/cap_dir_tree_for.rb
@@ -10,6 +10,8 @@ define :cap_dir_tree_for do
     /home/#{user}/#{app_name}/shared/log
     /home/#{user}/#{app_name}/shared/tmp
     /home/#{user}/#{app_name}/shared/tmp/pids
+    /home/#{user}/#{app_name}/shared/pids
+    /home/#{user}/#{app_name}/shared/sockets
   ).each do |path|
     directory path do
       owner user
